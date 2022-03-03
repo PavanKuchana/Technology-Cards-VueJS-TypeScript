90a7fb4b88d2aab7d6a5d443efc5100063d265e0
PavanKuchana <pavankuchana007@gmail.com>
2022-03-03 19:29:01 +0530

    Technology Cards VueJS-TypeScript

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,138 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+<div class="cards-app-container">
+    <div class="cards-list-container">
+      <h1 class="heading">Learn 4.0 Technologies</h1>
+      <p class="description">
+        Get trained by alumni of IITs and top companies like Amazon, Microsoft,
+        Intel, Nvidia, Qualcomm, etc. Learn directly from professionals involved
+        in Product Development.
+      </p>
+      <div class="cards-list">
+        <div v-for="each in cardsList" :key="each.id">
+          <CardItem :cardDetails="each" />
+          </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
+import { defineComponent, ref } from 'vue';
+import CardItem from './components/CardItem.vue';
+type data={
+  id:number,
+  title:string,
+  description:string,
+  imgUrl:string,
+  className:string
+}
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    CardItem
+  },
+  setup(){
+    const cardsList:data[] = [
+  {
+    id: 1,
+    title: 'Data Scientist',
+    description:
+      'Data scientists gather and analyze large sets of structured and unstructured data',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/data-scientist-img.png',
+    className: 'card-1',
+  },
+  {
+    id: 2,
+    title: 'IOT Developer',
+    description:
+      'IoT Developers are professionals who can develop, manage, and monitor IoT devices.',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/iot-developer-img.png',
+    className: 'card-2',
+  },
+  {
+    id: 3,
+    title: 'VR Developer',
+    description:
+      'A VR developer creates completely new digital environments that people can see.',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/vr-developer-img.png',
+    className: 'card-3',
+  },
+  {
+    id: 4,
+    title: 'ML Engineer',
+    description:
+      'Machine learning engineers feed data into models defined by data scientists.',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/ml-engineer-img.png',
+    className: 'card-4',
+  },
+]
+
+
+  return {cardsList}
   }
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+
+.cards-app-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f4faff;
+  min-height: 100vh;
+}
+
+.cards-list-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 80%;
+}
+
+.heading {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #171f46;
+  font-family: 'Roboto';
+  font-size: 48px;
+  font-weight: bold;
+}
+
+.description {
+  text-align: center;
+  color: #64748b;
+  font-family: 'Roboto';
+  font-size: 18px;
+  margin-top: 24px;
+  max-width: 625px;
+}
+
+.cards-list {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  margin-top: 32px;
+  max-width: 1100px;
+  padding-left: 0px;
+}
+
+@media screen and (min-width: 768px) {
+  .cards-list {
+    flex-direction: row;
+  }
 }
 </style>
---
+++ src/components/CardItem.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="card-item">
+       <div :class = "cardDetails.className">
+      <h1 class="card-title">{{cardDetails.title}}</h1>
+      <p class="card-description">{{cardDetails.description}}</p>
+      <div class="img-container">
+        <img class="card-img" :src="cardDetails.imgUrl" :alt="cardDetails.title" />
+      </div>
+      </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { PropType } from '@vue/runtime-core'
+type data={
+  id:number,
+  title:string,
+  description:string,
+  imgUrl:string,
+  className:string
+}
+export default defineComponent({
+    name:'CardItem',
+    props:{
+        cardDetails:{
+            type:Object as PropType<data>,
+            required:true
+        }
+    },
+})
+</script>
+
+
+<style scoped >
+.card-item {
+  background-color: #ffffff;
+  border-radius: 20px;
+  padding-left: 24px;
+  padding-right: 24px;
+  padding-top: 32px;
+  padding-bottom: 32px;
+  margin-left: 24px;
+  margin-right: 24px;
+  margin-bottom: 48px;
+  max-width: 323px;
+  list-style-type: none;
+}
+
+.card-1 {
+  border-top: 5px solid #ff4f64;
+}
+
+.card-2 {
+  border-top: 5px solid #00a8e7;
+}
+
+.card-3 {
+  border-top: 5px solid #44c4a1;
+}
+
+.card-4 {
+  border-top: 5px solid #fcc200;
+}
+
+.card-title {
+  color: #171f46;
+  font-family: 'Roboto';
+  font-size: 18px;
+  font-weight: 500;
+}
+
+.card-description {
+  color: #64748b;
+  font-family: 'Roboto';
+  font-size: 14px;
+}
+
+.img-container {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.card-img {
+  width: 80px;
+  height: 80px;
+}
+</style>
